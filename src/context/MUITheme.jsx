1385940badbd8deafd2d775a7c90d7bc18a9d794
Ali <alimoosabadi326@gmail.com>
2024-03-01 22:22:18 +0330

    adding the tailwind to the project and integrate it with MUI

--- src/context/MUITheme.jsx
+++ src/context/MUITheme.jsx
@@ -1,10 +1,11 @@
 import createCache from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { StyledEngineProvider, ThemeProvider, createTheme } from "@mui/material/styles";
 import PropTypes from "prop-types";
 import { prefixer } from "stylis";
 import rtlPlugin from "stylis-plugin-rtl";
+const rootElement = document.getElementById("root");
 const theme = createTheme({
   typography: {
     fontFamily: '"Vazir","Roboto","Arial"',
@@ -19,20 +20,45 @@ const theme = createTheme({
       main: "#a62626",
     },
   },
+  components: {
+    MuiPopover: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiPopper: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiDialog: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiModal: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+  },
 });
 const cacheRtl = createCache({
   key: "muirtl",
   stylisPlugins: [prefixer, rtlPlugin],
+  prepend:true,
 });
 
 function ThemeContext({ children }) {
   return (
-    <CacheProvider value={cacheRtl}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
-    </CacheProvider>
+    <StyledEngineProvider injectFirst>
+      <CacheProvider value={cacheRtl}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </CacheProvider>
+    </StyledEngineProvider>
   );
 }
 ThemeContext.propTypes = {
