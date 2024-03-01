3f5b374851d857c1ac452583de2319136f76116a
Ali <alimoosabadi326@gmail.com>
2024-03-01 19:08:42 +0330

    setting up the project..

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
-
 import App from "./App.jsx";
-
+import { ThemeContext } from "./context/MUITheme.jsx";
 import "./styles/index.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retryOnMount: false,
+      retry: 1,
+      retryDelay: 3000,
+      gcTime: 30 * 60 * 1000, // half an hour caches the data (default : 5min)
+      staleTime: 60 * 60 * 1000, // this option won't refetch teh data until the 60 mins(default: 0 second)
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ThemeContext>
+      <App />
+    </ThemeContext>
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 );
